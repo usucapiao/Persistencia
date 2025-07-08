9b9020f121563a7177b0d959b6d6e52ebb3cdd0a
usucapiao <12176559+usucapiao@users.noreply.github.com>
2025-07-08 18:52:33 -0400

    Delete src directory

--- src/main/java/edu/ifmt/cobrancaifmt/Titulos.java
+++
@@ -1,9 +0,0 @@
-package edu.ifmt.cobrancaifmt;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import edu.ifmt.cobrancaifmt.model.Titulo;
-
-public interface Titulos extends JpaRepository<Titulo, Long> {
-
-}
