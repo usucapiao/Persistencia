dc7f494cc7ae1c11cf31d2ae38c42cb38643cced
usucapiao <12176559+usucapiao@users.noreply.github.com>
2025-07-08 18:51:34 -0400

    Delete src/main/java/edu/ifmt/colecaodejogos directory

--- src/main/java/edu/ifmt/colecaodejogos/repository/Jogos.java
+++
@@ -1,8 +0,0 @@
-package edu.ifmt.colecaodejogos.repository;
-
-import edu.ifmt.colecaodejogos.model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Jogos extends JpaRepository<Jogo, Long> {
-
-}
